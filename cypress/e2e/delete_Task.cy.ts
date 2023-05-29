8deba15cf13460eb54babd5d6f3be2ee2df7ad8d
ibra101408 <danikvod@gmail.com>
2023-05-29 18:27:48 +0300

    185286520 delete task

---
+++ cypress/e2e/delete_Task.cy.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('deleteTask', () => {
+    it("should delete a user's task", () => {
+    cy.visit('http://localhost:8080/');
+            cy.contains('a', 'Sign In').click();
+            cy.get('#signin-email').click();
+            cy.get('#signin-email').type('existing_user');
+            cy.get('#signin-password').click();
+            cy.get('#signin-password').type('1234');
+            cy.get('#signin-submit').click();
+            cy.url().should('contains', 'http://localhost:8080/todos');
+            // Check if the text "delete me" exists and delete the item
+            cy.contains('.list-wrapper li', 'delete me', { matchCase: false })
+                .then(($item) => {
+                    if ($item.length > 0) {
+                        $item.find('.delete-btn').click();
+                    } else {
+                        // Handle the case when the text "delete me" is not found
+                        cy.log("Text 'delete me' not found");
+                    }
+                });
+    });
+});
